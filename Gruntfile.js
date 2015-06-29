dff38ce019c735078b3dc86edc838e8ff2529adc
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2015-06-29 13:58:28 -0400

    Initial commit, copied files from sparkpost-elite DEV repo

---
+++ Gruntfile.js
@@ -0,0 +1,154 @@
+var matchdep = require('matchdep')
+    , fs = require('fs')
+    , q = require('q')
+    , request = require('request')
+    , services = [
+        'introduction.md',
+        'substitutions_reference.md',
+        'metrics_api.md',
+        'recipient_list_api.md',
+        'sending_domains_api.md',
+        'suppression_list_api.md',
+        'templates_api.md',
+        'transmissions_api.md',
+        'webhooks_api.md',
+        'smtp_api.md'
+    ];
+
+module.exports = function(grunt) {
+    // Dynamically load any preexisting grunt tasks/modules
+    matchdep.filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // Configure existing grunt tasks and create custom ones
+    grunt.initConfig({
+        concat: {
+            options: {
+                banner: 'FORMAT: X-1A' + grunt.util.linefeed +
+                    'HOST: https://api.sparkpost.com/api/v1' +
+                    grunt.util.linefeed + grunt.util.linefeed
+            },
+            prod: {
+                src: services.map(function(s) { return 'services/' + s; }),
+                dest: 'apiary.apib'
+            }
+        },
+        connect: {
+            apiary: {
+                options: {
+                    port: 4000,
+                    hostname: '0.0.0.0',
+                    open: true,
+                    middleware: function(connect) {
+                        return [
+                            require('connect-livereload')(),
+                            connect.static('apiary-previews'),
+                            connect.directory('apiary-previews')
+                        ];
+                    }
+                }
+            }
+        },
+        shell: {
+            test: {
+                command : function(file) {
+                    if (file) {
+                        file = './services/' + file
+                    } else {
+                        file = 'apiary.apib'
+                    }
+                    return 'node ./bin/api-blueprint-validator ' + file;
+                },
+                options : {
+                    stdout : true,
+                    stderr: false,
+                    failOnError : true
+                }
+            }
+        },
+        watch: {
+            apiaryDocs: {
+                files: [ 'services/*.md' ],
+                tasks: [ 'generate-apiary-preview' ],
+                options: {
+                    livereload: true
+                }
+            }
+        }
+    });
+
+    /**
+     * Generates an apiary preview for an .md file
+     * @param file The .md file
+     * @returns {*|promise}
+     */
+    function generatePreview(file) {
+        var deferred = q.defer()
+            , options = {
+                url: 'https://api.apiary.io/blueprint/generate',
+                headers: {
+                    'Accept': 'text/html',
+                    'Content-Type': 'text/plain'
+                }
+            };
+
+        options.body = 'FORMAT: X-1A' + 
+            grunt.util.linefeed + 
+            'HOST: https://api.sparkpost.com/api/v1' + 
+            grunt.util.linefeed + grunt.util.linefeed + 
+            '# SparkPost API v1' + 
+            grunt.util.linefeed +
+            fs.readFileSync('./services/' + file, 'utf-8');
+    
+        request.post(options, function(err, response, body) {
+            var output = file.split('\.')[0] + '.html';
+            
+            if (err || response.statusCode !== 200) {
+                grunt.log.error('There was an error trying to generate the preview for ' + file, err, response.statusCode, response.body);
+                return deferred.reject(err || response.body);
+            } else {
+                fs.writeFile('./apiary-previews/' + output, body, function(err) {
+                    if (err) {
+                        grunt.log.error('There was an error trying to write to apiary.html', err);
+                        return deferred.reject(err);
+                    }
+                    return deferred.resolve();
+                });
+            }
+        });
+        
+        return deferred.promise;
+    }
+
+    // grunt generate-apiary-preview - creates apiary previews for all meta 
+    grunt.registerTask('generate-apiary-preview', 'Creates preview files for all md files in services', function() {
+        var done = this.async();
+        try {
+            fs.mkdirSync('./apiary-previews');
+        } catch(e){}
+        
+        fs.readdir('./services', function(err, files) {
+            q.all(files.map(generatePreview)).then(done, done);
+        });
+    });
+
+    // grunt testFiles - runs apiary-blueprint-validator on individual blueprint files
+    grunt.registerTask('testFiles', 'Validates individual blueprint files', services.map(function(s) {
+        return 'shell:test:' + s;
+    }));
+
+    //grunt preview - creates a live-reloaded preview of the docs, Apiary-style
+    grunt.registerTask('preview', 'View the apiary generated HTML files in the browser with all that live-reload goodness', [
+        'generate-apiary-preview',
+        'connect:apiary',
+        'watch:apiaryDocs'
+    ]);
+
+    // grunt test - runs apiary-blueprint-validator on combined apiary.apib file
+    grunt.registerTask('test', [ 'shell:test' ]);
+
+    // grunt compile - concatenates all the individual blueprint files and validates it
+    grunt.registerTask('compile', [ 'concat:prod', 'test' ]);
+
+    // register default grunt command as grunt test
+    grunt.registerTask('default', [ 'testFiles' ]);
+};
---
+++ README.md
@@ -0,0 +1,54 @@
+# SparkPost API Documentation
+
+## Prerequisites
+
+* You have installed [Git](http://git-scm.com/downloads) on your development machine
+* You have installed [Node.js and NPM](https://nodejs.org/) on your development machine
+
+The SparkPost API Docs in Apiary Blueprint format (based on Markdown)
+
+## Installation
+
+1. Clone the repository
+
+```git clone https://github.com/SparkPost/sparkpost-api-documentation```
+
+2. Change into the directory
+
+2. We use [Grunt](http://gruntjs.com/) for our task runner, so you will also have to install Grunt globally
+
+```npm install -g grunt-cli```
+
+3. Install the dependencies
+
+```npm install```
+
+## Development
+
+### Grunt Commands
+
+#### Apiary Blueprint Validator
+
+Once all the dependencies are installed, you can execute the Apiary Blueprint Validator tests in the following ways:
+
+* Run the test on ALL /services/ files sequentially
+  ```grunt testFiles```
+
+* Run the test an individual /services/ file
+  ```grunt shell:test:<filename>```
+
+#### Compile and Test
+
+* Concatenate ALL /services/ files into a single apiary.apib file, then test the compiled file
+  ```grunt compile```
+
+### Deploying to Production
+
+For complete directions on how to deploy to production, please view this [Confluence page on how to deploy our docs](https://confluence.int.messagesystems.com/display/PD/SparkPost.com+API+Documentation+on+Github)
+
+### Contributing
+[Guidelines for adding issues](docs/ADDING_ISSUES.markdown)
+
+[Apiary Blueprint Specification](https://github.com/apiaryio/api-blueprint/blob/master/API%20Blueprint%20Specification.md)
+
+[Submitting pull requests](docs/CONTRIBUTING.markdown)
